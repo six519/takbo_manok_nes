0dc102ee7a72a3686cc01ec2e058cdd3358e6e37
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2020-07-19 11:33:33 +0800

    change title

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "libs/neslib.h"
 #include "libs/nesdoug.h"
-#include "title.h"
+#include "game.h"
 #include "main.h" 
 
 #pragma bss-name(push, "ZEROPAGE")
@@ -12,33 +12,73 @@ const unsigned char bg_palette[]={
     0,0,0,0
 };
 
-void write_text(unsigned char text[], int x, int y) {
-    unsigned char i;
-    vram_adr(NTADR_A(x,y));
+const unsigned char sprite_palette[]={
+    ORANG, DK_GR, BLACK, LT_GY,
+    0,0,0,0,
+    0,0,0,0,
+    0,0,0,0
+};
 
-    i = 0;
-    while(text[i]){
-        vram_put(text[i]);
-        ++i;
+int game_state = 0;
+unsigned int default_scroll = 100;
+unsigned int scroll_x;
+
+void scroll_bg() {
+    if (game_state == 0) {
+        scroll_x = default_scroll;
+    } else {
+        scroll_x += 1;
+    }
+
+    set_scroll_x(scroll_x);
+}
+
+void draw_sprites(int * opp) {
+    oam_clear();
+    if (game_state == 0) {
+        // 8 sprites only per horizontal line :( sad
+        if (*opp) {
+            *opp = 0;
+            oam_meta_spr(80, 10, spriteT);
+            oam_meta_spr(80 + (16 * 2), 10, spriteK);
+            oam_meta_spr(80 + (16 * 4), 10, sprite_O);
+            oam_meta_spr(80 + 16, 10 + 16, spriteA);
+            oam_meta_spr(80 + (16 * 3), 10 + 16, sprite_O);
+        } else {
+            *opp = 1;
+            oam_meta_spr(80 + 16, 10, spriteA);
+            oam_meta_spr(80 + (16 * 3), 10, spriteB);
+            oam_meta_spr(80, 10 + 16, spriteM);
+            oam_meta_spr(80 + (16 * 2), 10 + 16, spriteN);
+            oam_meta_spr(80 + (16 * 4), 10 + 16, spriteK);
+        }
+
+    } else {
+
     }
 }
 
 void main (void) {
+    int opposite = 0;
     
     ppu_off();
 
     pal_bg(bg_palette);
+    pal_spr(sprite_palette);
 
     vram_adr(NAMETABLE_A);
-    vram_unrle(title);
+    vram_unrle(game);
+    vram_adr(NAMETABLE_B);
+    vram_unrle(game);
 
     music_play(0);
-
-    write_text("Press Start", 42, 146);
     ppu_on_all();
     
     
-    while (1){   
+    while (1){
+        ppu_wait_nmi();
+        scroll_bg();
+        draw_sprites(&opposite);
     }
 }
     
--- main.h
+++ main.h
@@ -8,3 +8,60 @@
 #define LT_GR 0x3a
 #define DK_GR 0x0b
 #define MD_YL 0x28
+#define ORANG 0x16
+
+const unsigned char spriteT[]={
+      0,  0,0x80,0,
+      0,  8,0x90,0,
+      8,  0,0x81,0,
+      8,  8,0x91,0,
+    128
+};
+
+const unsigned char spriteA[]={
+      0,  0,0xa0,0,
+      0,  8,0xb0,0,
+      8,  0,0xa1,0,
+      8,  8,0xb1,0,
+    128
+};
+
+const unsigned char spriteK[]={
+      0,  0,0x82,0,
+      0,  8,0x92,0,
+      8,  0,0x83,0,
+      8,  8,0x93,0,
+    128
+};
+
+const unsigned char spriteB[]={
+      0,  0,0xa2,0,
+      0,  8,0xb2,0,
+      8,  0,0xa3,0,
+      8,  8,0xb3,0,
+    128
+};
+
+const unsigned char sprite_O[]={
+      0,  0,0x84,0,
+      0,  8,0x94,0,
+      8,  0,0x85,0,
+      8,  8,0x95,0,
+    128
+};
+
+const unsigned char spriteM[]={
+      0,  0,0xa4,0,
+      0,  8,0xb4,0,
+      8,  0,0xa5,0,
+      8,  8,0xb5,0,
+    128
+};
+
+const unsigned char spriteN[]={
+      0,  0,0x86,0,
+      0,  8,0x96,0,
+      8,  0,0x87,0,
+      8,  8,0x97,0,
+    128
+};
